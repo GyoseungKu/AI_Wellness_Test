53f0c87deba2e2c63cd203f81663a241eb40d7da
Gyoseung Ku <106974512+GyoseungKu@users.noreply.github.com>
2026-01-31 10:44:44 +0900

    Add files via upload

---
+++ src/main/java/org/syu_likelion/AI_Wellness_Test/AiWellnessTestApplication.java
@@ -0,0 +1,13 @@
+package org.syu_likelion.AI_Wellness_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWellnessTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWellnessTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/syu_likelion/AI_Wellness_Test/AiWellnessTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syu_likelion.AI_Wellness_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWellnessTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
